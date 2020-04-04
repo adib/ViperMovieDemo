2e5d0f8f3dcdc73df9ef7334591a39b76e41cbd9
Sasmito Adibowo <adib@basil-salad.com>
2020-04-04 17:27:23 +0800

    Initial Commit

---
+++ ViperMovieDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperMovieDemo
+//
+//  Created by Sasmito Adibowo on 4/4/20.
+//  Copyright © 2020 Basil Salad Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ViperMovieDemoTests/ViperMovieDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ViperMovieDemoTests.swift
+//  ViperMovieDemoTests
+//
+//  Created by Sasmito Adibowo on 4/4/20.
+//  Copyright © 2020 Basil Salad Software. All rights reserved.
+//
+
+import XCTest
+@testable import ViperMovieDemo
+
+class ViperMovieDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
